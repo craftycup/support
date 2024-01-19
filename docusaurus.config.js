7fb374226f9290566fa36b96d5b9188730138d72
craftycup <114754355+craftycup@users.noreply.github.com>
2024-01-19 17:10:25 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
       },
       prism: {
         theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        darkTheme: prismThemes.dracula, 
       },
     }),
 };
